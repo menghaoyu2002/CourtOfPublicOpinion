9121d5fac9e740e2348b590c9808d6239269d8ab
Menghao Yu <menghaoyu2002@gmail.com>
2023-09-02 00:13:58 -0400

    initial go rod setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"regexp"
 
+	"github.com/go-rod/rod"
 	"github.com/labstack/echo/v4"
 )
 
+var browser = rod.New()
+
 func main() {
-    e := echo.New()
-    e.GET("/:num", func(c echo.Context) error {
-        return c.String(http.StatusOK, c.Param("num"))
-    })
-    e.Logger.Fatal(e.Start(":3000"))
+	e := echo.New()
+	e.GET("/search", handleSearch)
+	e.Logger.Fatal(e.Start(":3000"))
 }
 
+func handleSearch(context echo.Context) error {
+	s := context.QueryParam("search_query")
+
+	if s == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "Missing Search String")
+	}
+
+	youtubeUrl := "https://www.youtube.com/results?search_query=%s"
+	yt := fmt.Sprintf(youtubeUrl, s)
+
+	page := browser.MustConnect().MustPage(yt)
+	defer page.MustClose()
+
+	links := page.MustWaitDOMStable().MustElements("a#video-title")
+	for i := 0; i < len(links); i++ {
+		link := links[i].MustProperty("href").String()
+
+        // ignore youtube shorts
+		if matched, _ := regexp.Match(`.*/shorts/.*`, []byte(link)); !matched {
+			go visitVideo(browser, link)
+		}
+	}
+
+	return context.String(http.StatusOK, yt)
+}
+
+func visitVideo(browser *rod.Browser, link string) {
+	page := browser.MustConnect().MustPage(link)
+	defer page.MustClose()
+
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module courtOfPublicOpinion/api
 
 go 1.21
 
-require github.com/labstack/echo/v4 v4.11.1
+require (
+	github.com/go-rod/rod v0.114.3
+	github.com/labstack/echo/v4 v4.11.1
+)
 
 require (
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -10,8 +13,13 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
-	golang.org/x/net v0.12.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
+	github.com/ysmood/fetchup v0.2.3 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.34.2 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 )
